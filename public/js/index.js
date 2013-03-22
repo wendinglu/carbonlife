4139dedc2ec50bee3471f2eeee144bca83f57140
wendinglu <wending@stanford.edu>
2013-03-21 21:09:01 -0700

    translation logic works

--- public/js/index.js
+++ public/js/index.js
@@ -8,26 +8,56 @@ function inputFieldListener() {
 	$("#test-field").text(numericString);
 }
 
-function submit() {
-	$("#results").removeClass("hidden");
-
-	var param = praseFloat($("#co2-form").val());
-
-}
-
 function getURLParameter(name) {
     return decodeURI(
         (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
     );
 }
 
-var carbonlife = {
-	massConversion: {
+var unitConversion = {
+	constants: {
 		"kilograms": 1,
-		"metric tonnes": .001,
-		
-	}
+		"metric tonnes": 1e+3,
+		"kilotonnes": 1e+6,
+		"Megatonnes": 1e+9,
+		"Gigatonnes": 1e+12,
+		"pounds": 0.453592,
+		"tons": 907.185
+	},
 
+	convertToKg: function(num, units) {
+		if (!this.constants[units]) units = "kilograms";
+		return (num * this.constants[units]);
+	},
+
+	reduceByMagnitude: function(num, magnitude) {
+		num /= magnitude;
+		num = num.toFixed(0);
+		return num;
+	},
+
+	wordify: function(num) {
+		if (num >= 1e20) {
+			num = this.reduceByMagnitude(num, 1e20) + "gajillion";
+		} else if (num >= 1e16) {
+			num = this.reduceByMagnitude(num, 1e16) + "quadrillion";
+		} else if (num >= 1e12) {
+			num = this.reduceByMagnitude(num, 1e12) + " trillion";
+		} else if (num >= 1e9) {
+			num = this.reduceByMagnitude(num, 1e9) + " billion";
+		} else if (num >= 1e6) {
+			num = this.reduceByMagnitude(num, 1e6) + " million";
+		} else if (num >= 1e3) {
+			num = this.reduceByMagnitude(num, 1e3) + " thousand";
+		} else {
+			num = this.reduceByMagnitude(num, 1);
+		}
+
+		return num;
+	}
+}
+
+var carbonlife = {
 	equivTable: {
 		"Foods": {
 			"oranges": .09
@@ -87,15 +117,14 @@ var carbonlife = {
 		"Nature": {
 			"Mt. St. Helens eruptions": 1e+7
 			, "days of Eyjafjallajkull": 150e+6
-			, "years of Yellowstone": 44e+9
 			, "years of every volcano on Earth": 2e+8
 		},
 
 		"Global Emissions": {
-			"% of India's 2010 emissions": 2.07e+12
-			, "% of USA's 2010 emissions": 5.492e+12
-			, "% of China's 2010 emissions": 8.241e+12
-			, "% of World 2010 emissions": 33.509e+12
+			"% of India's 2010 emissions": 2.07e+12/100
+			, "% of USA's 2010 emissions": 5.492e+12/100
+			, "% of China's 2010 emissions": 8.241e+12/100
+			, "% of World 2010 emissions": 33.509e+12/100
 		}
 	},
 
@@ -109,20 +138,49 @@ var carbonlife = {
 		});
 
 		//If we have a parameter, load that parameter
-		// var params = parseFloat($("#params").text());
 		var params = parseFloat(getURLParameter("amount"));
 		if (params) {
 			$("#co2-input").val(params);
-			submit();
+			this.submit();
 		}
 	},
 
-	generateResults: function() {
+	submit: function() {
+		$("#results").removeClass("hidden");
+
+		//convert to kilograms
+		var params = parseFloat(getURLParameter("amount"));
+		var units = $("#co2-select").val();
+		var kg = unitConversion.convertToKg(params, units);
+		this.generateResults(kg);
+	},
+
+	generateResults: function(input) {
+		var listStart = "<ul class='results'>";
+		var listEnd = "</ul>";
+		var lineStart = "<li class='results'>";
+		var lineEnd = "</li>";
+
+		for (var category in this.equivTable) {
+			$("#results").append("<h3>" + category + "</h3>");
+			$("#results").append(listStart);
+			for (var itemKey in this.equivTable[category]) {
+				var num = input/this.equivTable[category][itemKey];
+				if (num > 0.1) {
+					num = unitConversion.wordify(num);
+					var printout = lineStart + "<strong>" + num + "</strong> " +
+					itemKey + lineEnd;
+
+					$("#results").append(printout);
+				}
+			}
+
+			$("#results").append(listEnd);
+		}
 	}
 }
 
 $(document).ready(function() {
 	carbonlife.start();
 	var input = document.getElementById("co2-form").elements["amount"];
-	$("#test-field").text(input.value);
 });
